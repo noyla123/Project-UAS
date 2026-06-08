cbb20b66c015b53a91893dd6b481801c034c4fd6
noyla123 <aestetoqnoyla@gmail.com>
2026-06-08 10:10:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request
+import hashlib
 
-application = Flask(__name__)
+application = Flask(**name**)
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
-   if request.method == 'POST':
-      namaDepan = request.form['namaDepan']
-      namaBelakang = request.form['namaBelakang']
-      nama = '%s %s' % (namaDepan, namaBelakang)
-      return render_template('response.html', nama=nama)
-   return render_template('form.html')
 
-if __name__ == '__main__':
-   application.run(debug=True)
+```
+if request.method == 'POST':
+
+    file = request.files['sertifikat']
+
+    if file:
+
+        isi_file = file.read()
+
+        hash_sha256 = hashlib.sha256(isi_file).hexdigest()
+
+        return render_template(
+            'response.html',
+            hash=hash_sha256
+        )
+
+return render_template('form.html')
+```
+
+if **name** == '**main**':
+application.run(debug=True)
