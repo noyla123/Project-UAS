df04032b613c0897f35fec6f0c60c05205e77f9f
noyla123 <aestetoqnoyla@gmail.com>
2026-06-08 10:27:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 from flask import Flask, render_template, request
 import hashlib
 
-application = Flask(**name**)
+application = Flask(__name__)
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
 
-```
 if request.method == 'POST':
-
     file = request.files['sertifikat']
-
     if file:
-
         isi_file = file.read()
-
         hash_sha256 = hashlib.sha256(isi_file).hexdigest()
-
         return render_template(
             'response.html',
             hash=hash_sha256
         )
-
 return render_template('form.html')
-```
 
-if **name** == '**main**':
+if __name__ == '__main__':
 application.run(debug=True)
